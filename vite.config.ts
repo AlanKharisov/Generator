b90d0d897460ecbd8462db1765c307b784627d4b
AlanKharisov <alankharisov1@gmail.com>
2026-02-03 18:15:27 +0100

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Generator' // имя твоего репозитория
+  base: '/Generator/' // обязательно имя репозитория
 })
