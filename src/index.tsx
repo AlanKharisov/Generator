9980064fd6888bc537b9d66bfeb1fd448a30c3b2
AlanKharisov <alankharisov1@gmail.com>
2026-02-03 19:17:18 +0100

    Refactor React rendering to use createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
-import './index.css';
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
