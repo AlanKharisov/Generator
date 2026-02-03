8d2cdc578cc75af2ec2750ab616da426fd8a4485
AlanKharisov <alankharisov1@gmail.com>
2026-02-03 18:38:40 +0100

    Update base path comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Generator/' // имя репозитория на GitHub Pages
+  base: '/Generator/' // <--- строго совпадает с названием репозитория
 })
